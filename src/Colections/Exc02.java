d879fbe6c717455dc2b50af51acd7984411f8bb1
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-06-10 19:47:52 -0300

    Exercicios Collections

---
+++ src/Colections/Exc02.java
@@ -0,0 +1,37 @@
+package Colections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exc02 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int myNum, count = 0;
+        boolean find = false;
+        myList.add(2);
+        myList.add(5);
+        myList.add(1);
+        myList.add(3);
+        myList.add(4);
+        myList.add(9);
+        myList.add(7);
+        myList.add(8);
+        myList.add(10);
+        myList.add(6);
+
+        System.out.println("Digite o  número que deseja encontrar: ");
+        myNum = sc.nextInt();
+        for (int i : myList) {
+            if (myNum == i) {
+                System.out.println("O número " + myNum + " está localizado na posição: " + count);
+                find = true;
+                break;
+            }
+            count++;
+        }
+        if (!find) {
+            System.out.println("O número " + myNum + " não foi encontrado!");
+        }
+    }
+}
